96d72b6e71d47d85f1a1d03e980c952d05a20fb4
wilhsie <wjhsie@gmail.com>
2015-12-18 16:23:05 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#Welcome to NUGDS Analytics Tools Repo
+
+To run the python script in /json_parser run using python on the test data:
+
+i.e.
+
+python mixed_reality_json_parser.py 12-18-2015_test_data.json
---
+++ json_parser/mixed_reality_json_parser.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env
+
+"""
+
+    Hello and welcome to the mixed_reality_json_parser.py tool >^ o ^<
+    
+    Input:
+        JSON containing player data created by Mixed Reality
+    Output:
+        Player name / id
+        List of (player action, client time)
+
+
+"""
+
+import sys
+import json
+
+fd = open(sys.argv[1])
+
+# TODO: Handle case where line != type(JSON) i.e. line = # version:1
+
+for line in fd:
+    json_dump = json.dumps(line)
+    json_load = json.loads(json_dump)
+    json_reload = json.loads(json_load)
+    if(json_reload['type'] == "action"):
+        print json_reload['data']['details'], json_reload['data']['client_time']
+
